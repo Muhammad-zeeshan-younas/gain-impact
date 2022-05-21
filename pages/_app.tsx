0884896e02cde49f3aec6ef9fbaf50d8c2280a66
Muhammad Zeeshan Younas <muhammadzeeshanyounas777@gmail.com>
2022-05-21 08:47:01 -0700

    job

---
+++ pages/_app.tsx
@@ -0,0 +1,36 @@
+import '../styles/globals.css'
+import type { AppProps } from 'next/app'
+import { parseCookies } from './parseCookie'
+import { AuthContextProvider } from '../context/authContext'
+import Router, { useRouter } from 'next/router'
+import { useState, useCallback, useEffect } from 'react'
+import NProgress from 'nprogress' //nprogress module
+import 'nprogress/nprogress.css' //styles of nprogress
+
+//Binding events.
+function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter()
+  const [isLoading, setIsLoading] = useState(false)
+  useEffect(() => {
+    const handleRouteStart = () => NProgress.start()
+    const handleRouteDone = () => NProgress.done()
+
+    Router.events.on('routeChangeStart', handleRouteStart)
+    Router.events.on('routeChangeComplete', handleRouteDone)
+    Router.events.on('routeChangeError', handleRouteDone)
+
+    return () => {
+      // Make sure to remove the event handler on unmount!
+      Router.events.off('routeChangeStart', handleRouteStart)
+      Router.events.off('routeChangeComplete', handleRouteDone)
+      Router.events.off('routeChangeError', handleRouteDone)
+    }
+  }, [])
+  return (
+    <AuthContextProvider>
+      <Component {...pageProps} />
+    </AuthContextProvider>
+  )
+}
+
+export default MyApp
---
+++ pages/index.js
@@ -0,0 +1,55 @@
+import { db } from '../firebase'
+import { collection, onSnapshot, query, where } from 'firebase/firestore'
+import Sidebar from '../components/sidebar/sidebar'
+import Chat from '../components/chat/chat'
+import { useContext, useEffect, useLayoutEffect, useState } from 'react'
+import { AuthContext } from '../context/authContext'
+import { useRouter } from 'next/router'
+const Home = () => {
+  const router = useRouter()
+  const [loading, setLoading] = useState(false)
+  const auth = useContext(AuthContext)
+  useEffect(() => {
+    setLoading(true)
+    if (!auth.user) {
+      router.replace('/signin')
+    }
+  }, [])
+  const [channel, setChannel] = useState([])
+
+  useEffect(() => {
+    const getData = async () => {
+      if (auth.user) {
+        onSnapshot(
+          query(
+            collection(db, 'channels'),
+
+            where('members', 'array-contains', auth.user.username)
+          ),
+          (snapshot) => {
+            setChannel(
+              snapshot.docs.sort(function (x, y) {
+                return y.data().recentActivity - x.data().recentActivity
+              })
+            )
+          }
+        )
+      }
+    }
+    getData()
+  }, [db])
+
+  if (!router.query.channel) {
+    router.query.channel = channel[0]?.id
+  }
+  return (
+    <div className="flex">
+      <Sidebar channel={channel} />
+      {router.query.channel && (
+        <Chat channel={router.query.channel} channels={channel} />
+      )}
+    </div>
+  )
+}
+
+export default Home
